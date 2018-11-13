b5b5137917038a7f7aac9245b1f24a9c8ff0b29a
popelkin <shurik.wps@gmail.com>
2018-11-13 10:40:15 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${request.method} ${request.url}`;
+
+    fs.appendFile('server.log', log + "\n", (error) => {
+        if (error) {
+            console.log('Unable to append log to file.');
+        }
+    });
+
+    next();
+});
+
+app.use((request, response, next) => {
+    response.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        title: 'Home Page',
+        message: 'Welcome to my website!'
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        title: 'About Page',
+    });
+});
+
+app.get('/bad', (request, response) => {
+    response.send({
+        errorMessage: "Unable to handle request"
+    });
+});
+
+app.listen(80, () => {
+    console.log('Server is up on port 80');
+});
